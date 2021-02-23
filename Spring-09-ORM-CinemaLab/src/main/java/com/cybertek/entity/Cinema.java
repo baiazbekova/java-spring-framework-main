cf442a18fe4229f31f8ff2a9d0aafe96e714adaf
baiazbekova <baiazbekova@gmail.com>
2021-02-23 13:05:27 -0500

    orm-lab-cinemaApp

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
