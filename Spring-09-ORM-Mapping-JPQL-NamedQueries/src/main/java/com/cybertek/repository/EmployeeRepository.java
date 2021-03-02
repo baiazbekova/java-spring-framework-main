e8bb59409e91a56ffa25ffaff9caa0f06a910164
baiazbekova <baiazbekova@gmail.com>
2021-03-02 12:44:30 -0500

    orm-jpql

---
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.cybertek.repository;
+
+import com.cybertek.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository <Employee,Long> {
+
+    @Query("SELECT e from Employee e where e.email='dtrail8@tamu.edu'")
+    Integer getEmployeeSalary();
+
+}
