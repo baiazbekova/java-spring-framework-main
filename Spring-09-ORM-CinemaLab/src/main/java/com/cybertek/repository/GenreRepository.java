f696e5f10944816b2c785a8b08ad98ba65e9b080
baiazbekova <baiazbekova@gmail.com>
2021-03-03 11:44:51 -0500

    practicing queries

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/GenreRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/GenreRepository.java
@@ -2,8 +2,22 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+    //JPQL
+
+    //Write a JPQL query tthat returns all genres
+    @Query(value="select g from Genre g")
+    List<Genre> readAllBy();
+
+    //Native query
+    //write a native query that returns genres by containing name
+    @Query(value = "select * from Genre where name ILIKE concat ('%', ?1, '%')", nativeQuery = true)
+    List<Genre> retrieveByName(String name);
 }
